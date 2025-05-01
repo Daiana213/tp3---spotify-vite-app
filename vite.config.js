62e2a188ccc8af53b010117f23bdc73c5f6ea47e
Daiana Palacios <daianapalacios213@gmail.com>
2025-05-01 14:23:01 -0300

    ghPages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: 'https://github.com/Daiana213/tp3---spotify-vite-app', 
   plugins: [react()]
 });
