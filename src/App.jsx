b5c2081afa1a1c993bde010ddad26e50db5801d9
Daiana Palacios <daianapalacios213@gmail.com>
2025-05-07 21:27:58 -0300

    Arreglar inicio

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,19 @@ import Header from './components/Header';
 
 const auth = localStorage.getItem("spotifyAuth");
 
+console.log(auth);
+
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/tp3---spotify-vite-app/'>
       <Header />
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/search" element={auth ? <ArtistSearch /> : <Navigate to="/login" />} />
         <Route path="/artist/:id" element={auth ? <ArtistDetail /> : <Navigate to="/login" />} />
         <Route path="/album/:id" element={auth ? <AlbumDetail /> : <Navigate to="/login" />} />
         <Route path="/favorites" element={auth ? <FavoriteSongs /> : <Navigate to="/login" />} />
-        <Route path="*" element={<Navigate to={auth ? "/search" : "/login"} />} />
       </Routes>
     </BrowserRouter>
   );
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/tp3---spotify-vite-app', 
+  base: '/tp3---spotify-vite-app/', 
   plugins: [react()]
 });
